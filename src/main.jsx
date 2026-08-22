32c58be0c3aba63752626681ad4d5fa2cb291a5d
Joshh031 <37054791+Joshh031@users.noreply.github.com>
2026-08-22 18:15:39 -0400

    Use deduplicating podcast capture UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './QuickAppV3.jsx'
+import App from './QuickAppV4.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
