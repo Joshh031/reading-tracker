1a0bd4f71c523ae1b0da66a7ba1c202718654572
Joshh031 <37054791+Joshh031@users.noreply.github.com>
2026-08-22 13:00:10 -0400

    Use reviewed PM takeaway interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './QuickAppV2.jsx'
+import App from './QuickAppV3.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
