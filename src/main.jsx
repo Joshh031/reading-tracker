2847e34493e2076a9c41f91fa171f29cbc69baf7
Joshh031 <37054791+Joshh031@users.noreply.github.com>
2026-08-22 12:51:45 -0400

    Use guest-first reading log UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './QuickApp.jsx'
+import App from './QuickAppV2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
