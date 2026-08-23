64069e5adbd1e34683cb39a79c1be30d00fa8d40
Joshh031 <37054791+Joshh031@users.noreply.github.com>
2026-08-23 09:49:52 -0400

    Use chapter-aware Reading Log UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './QuickAppV4.jsx'
+import App from './QuickAppV5.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
